fc64be8c5ff134d55e5096aa93e9e9d15ad5f08d
Jon Goodger <jonno.is@gmail.com>
2025-05-04 04:26:43 +0100

    Export the trainer

--- client/client.ts
+++ client/client.ts
@@ -5,3 +5,7 @@ import Trainer from "Trainer";
 export function Delay(ms: number): Promise<CitizenTimer> {
     return new Promise(res => setTimeout(res, ms, null));
 }
+
+const trainer = new Trainer();
+
+exports('virakalTrainer', () => trainer);

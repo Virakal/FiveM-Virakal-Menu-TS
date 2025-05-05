fe6225000fb6d835efbf0dc9ec499477bae13f14
Jon Goodger <jonno.is@gmail.com>
2025-05-05 10:57:23 +0100

    Implement server

--- server/server.ts
+++ server/server.ts
@@ -1,1 +1,125 @@
 console.log("[virakal-menu] Server Resource Started");
+
+import { mkdir, readFile, writeFile } from 'node:fs/promises';
+import { realpathSync } from 'node:fs';
+
+interface Time {
+    hours: number,
+    minutes: number,
+    seconds: number,
+}
+
+class Server {
+    configPath = 'virakal-configs/';
+    currentWeather: number = -1;
+    currentTime: Time
+
+    constructor() {
+        onNet('virakal:requestWeather', this.onRequestWeather.bind(this));
+        onNet('virakal:changeWeather', this.onChangeWeather.bind(this));
+
+        onNet('virakal:requestTime', this.onRequestTime.bind(this));
+        onNet('virakal:changeTime', this.onChangeTime.bind(this));
+
+        onNet('virakal:getConfig', this.onGetConfig.bind(this));
+        onNet('virakal:setConfig', this.onGetConfig.bind(this));
+
+        const realPath = realpathSync(this.configPath);
+        console.log(`Virakal Menu configs at ${realPath}`);
+        this.makeConfigDirectory();
+    }
+
+    onChangeWeather(weather: number) {
+        const name = GetPlayerName(source.toString());
+        this.currentWeather = weather;
+        // TODO: Nice name
+        console.log(`Weather changed to ${weather} by ${name}`);
+    }
+
+    onRequestWeather(weather: number) {
+        const sourceStr = source.toString();
+        const name = GetPlayerName(sourceStr);
+        // TODO Nice name
+        console.log(`Weather requested by ${name}. Weather is ${this.currentWeather}.`);
+        this.currentWeather = weather;
+
+        if (weather !== -1) {
+            emitNet('vitakal:setWeather', sourceStr, weather);
+        }
+    }
+
+    onChangeTime(hours: number, minutes: number, seconds: number) {
+        const name = GetPlayerName(source.toString());
+        this.currentTime = { hours, minutes, seconds };
+        // TODO: Nice name
+        console.log(`Weather changed to ${this.prettyTime} by ${name}`);
+    }
+
+    onRequestTime() {
+        const sourceStr = source.toString();
+        const name = GetPlayerName(sourceStr);
+        console.log(`Time requested by ${name}. Time is ${this.prettyTime}.`);
+
+        const time = this.currentTime;
+
+        if (time !== null) {
+            emitNet('virakal:setTime', sourceStr, time.hours, time.minutes, time.seconds);
+        }
+    }
+
+    async onGetConfig() {
+        const path = this.getConfigPathForPlayer(source);
+        const sourceName = GetPlayerName(source.toString());
+        const playerId = source.toString();
+        let contents = {};
+
+        try {
+            contents = JSON.parse((await readFile(path)).toString());
+        } catch {
+            console.log(`${sourceName} requested a config from file ${path} but it doesn't exist yet.`);
+            contents = {};
+        }
+
+        emitNet('virakal:returnConfig', playerId, contents);
+    }
+
+    async onSetConfig(config: any) {
+        const path = this.getConfigPathForPlayer(source);
+
+        await writeFile(path, config, {
+            flag: 'w',
+            mode: 0o644,
+        });
+    }
+
+    get prettyTime(): string {
+        const time = this.currentTime;
+
+        if (time === null) {
+            return '[none]';
+        }
+
+        return [time.hours, time.minutes, time.seconds].map((x) => ('00' + x).slice(-2)).join(':');
+    }
+
+    private async makeConfigDirectory() {
+        await mkdir(this.configPath);
+    }
+
+    private getConfigPathForPlayer(playerId: number) {
+        const handles = getPlayerIdentifiers(playerId).filter((s) => s.startsWith('license:'));
+        let handle;
+
+        if (GetConvar('sv_fxdkMode', '0') === '1') {
+            handle = 'developer';
+        } else if (handles.length === 0) {
+            handle = 'default';
+        } else {
+            handle = handles[0].replaceAll(':', '_');
+        }
+
+        return `${this.configPath.replace(/[/\\]$/, '')}/${handle}.json`;
+    }
+}
+
+new Server();

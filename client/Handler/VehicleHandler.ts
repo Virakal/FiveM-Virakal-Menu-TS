b52d7f18e1ebd8f83e3cf5fec5791437803bab93
Jon Goodger <jonno.is@gmail.com>
2025-05-10 13:34:24 +0100

    Implement veh command

--- client/Handler/VehicleHandler.ts
+++ client/Handler/VehicleHandler.ts
@@ -1,6 +1,9 @@
 import getConfig from "@common/Config";
-import { notify } from "@common/utils";
+import { SeatPosition } from "@common/Data/ParamEnums";
+import { delay, invertColour, notify, rainbowRgb } from "@common/utils";
 import type Trainer from "Trainer";
+
+const RAINBOW_TICK_DELAY = 100;
 
 export default class VehicleHandler implements Handler {
     private trainer;
@@ -26,7 +29,7 @@ export default class VehicleHandler implements Handler {
         });
 
         // // General
-        // RegisterNuiCallback('veh', this.onVeh.bind(this));
+        RegisterNuiCallback('veh', this.onVeh.bind(this));
         // RegisterNuiCallback('vehspawn', this.onVehSpawn.bind(this));
         // RegisterNuiCallback('vehsearch', this.onVehSearch.bind(this));
         // RegisterNuiCallback('vehseat', this.onVehSeat.bind(this));
@@ -63,14 +66,122 @@ export default class VehicleHandler implements Handler {
 
         // on('virakalMenu:newVehicle', this.onNewVehicle);
 
-        // setTick(this.rainbowTick.bind(this));
+        setTick(this.rainbowTick.bind(this));
         // setTick(this.boostTick.bind(this));
-        // setTick(this.invincibleCarTick.bind(this));
+        setTick(this.invincibleCarTick.bind(this));
         // setTick(this.checkChangedCar.bind(this));
 
         if (config.has('RainbowSpeed')) {
             this.rainbowSpeed = Number.parseFloat(config.get('RainbowSpeed')) ?? 0.5;
         }
+    }
+
+    onVeh(data: NuiData, cb: NuiCallback): NuiCallback {
+        const config = getConfig();
+        const { action, newstate: state } = data;
+        const ped = PlayerPedId();
+        const vehicle = GetVehiclePedIsUsing(ped);
+        const noVehicleMessage = '~r~Not in a vehicle!';
+
+        switch (action) {
+            case 'fix':
+                if (!vehicle) {
+                    notify(noVehicleMessage);
+                    break;
+                }
+
+                SetVehicleFixed(vehicle);
+                SetEntityHealth(vehicle, 1000);
+                notify('~g~Vehicle repaired.');
+                break;
+            case 'clean':
+                if (!vehicle) {
+                    notify(noVehicleMessage);
+                    break;
+                }
+
+                SetVehicleDirtLevel(vehicle, 0);
+                notify('~g~Vehicle cleaned.');
+                break;
+            case 'flip':
+                if (!vehicle) {
+                    notify(noVehicleMessage);
+                    break;
+                }
+
+                const success = SetVehicleOnGroundProperly(vehicle);
+
+                if (success) {
+                    notify('~g~Vehicle placed on ground.');
+                } else {
+                    notify('~r~Couldn\'t place the vehicle on the ground.');
+                }
+
+                break;
+            case 'boosthorn':
+                config.set('BoostOnHorn', state);
+
+                if (state) {
+                    notify('~g~Boost on horn enabled.');
+                } else {
+                    notify('~g~Boost on horn disabled.');
+                }
+
+                break;
+            case 'rainbowcar':
+                config.set('RainbowPaint', state);
+
+                if (state) {
+                    notify('~g~Rainbow paint enabled.');
+                } else {
+                    notify('~g~Rainbow paint disabled.');
+                }
+
+                break;
+            case 'rainbowcar':
+                config.set('RainbowChrome', state);
+
+                if (state) {
+                    notify('~g~Rainbow chrome enabled.');
+                } else {
+                    notify('~g~Rainbow chrome disabled.');
+                }
+
+                break;
+            case 'rainbowneon':
+                config.set('RainbowNeon', state);
+
+                if (state) {
+                    notify('~g~Rainbow neon enabled.');
+                } else {
+                    notify('~g~Rainbow neon disabled.');
+                }
+
+                break;
+            case 'rainbowneoninverse':
+                config.set('RainbowNeonInverse', state);
+
+                if (state) {
+                    notify('~g~Rainbow neon (inverse colours) enabled.');
+                } else {
+                    notify('~g~Rainbow neon (inverse colours) disabled.');
+                }
+
+                break;
+            case 'invincible':
+                config.set('InvincibleVehicle', state);
+
+                if (state) {
+                    notify('~g~Invincible vehicle enabled.');
+                } else {
+                    notify('~g~Invincible vehicle disabled.');
+                }
+
+                break;
+        }
+
+        cb('ok');
+        return cb;
     }
 
     onRainbowSpeed(data: NuiData, cb: NuiCallback): NuiCallback {
@@ -88,5 +199,51 @@ export default class VehicleHandler implements Handler {
 
         cb('ok');
         return cb;
+    }
+
+    async rainbowTick(): Promise<void> {
+        await delay(RAINBOW_TICK_DELAY);
+
+        const config = getConfig();
+        const ped = PlayerPedId();
+        const vehicle = GetVehiclePedIsUsing(ped);
+
+        if (!vehicle || GetPedInVehicleSeat(vehicle, SeatPosition.SF_FrontDriverSide) !== ped) {
+            return;
+        }
+
+        const rgb = rainbowRgb(this.rainbowSpeed);
+        const [r, g, b] = rgb;
+
+        if (config.getBool('RainbowPaint') || config.getBool('RainbowChrome')) {
+            SetVehicleCustomPrimaryColour(vehicle, r, g, b);
+            SetVehicleCustomSecondaryColour(vehicle, r, g, b);
+        }
+
+        if (config.getBool('RainbowNeonInverse')) {
+            const [ir, ig, ib] = invertColour(rgb)
+            SetVehicleNeonLightsColour(vehicle, ir, ig, ib);
+        } else if (config.getBool('RainbowNeon')) {
+            SetVehicleNeonLightsColour(vehicle, r, g, b);
+        }
+    }
+
+    async invincibleCarTick(): Promise<void> {
+        // TODO: We maybe don't have to do this every tick?
+        await delay(0);
+
+        const vehicle = GetVehiclePedIsUsing(PlayerPedId());
+
+        if (vehicle) {
+            const invincible = getConfig().getBool('InvincibleVehicle');
+
+            SetVehicleCanBeVisiblyDamaged(vehicle, !invincible);
+            SetVehicleTyresCanBurst(vehicle, !invincible);
+            SetVehicleWheelsCanBreak(vehicle, !invincible);
+            SetEntityInvincible(vehicle, invincible);
+            SetEntityProofs(vehicle, invincible, invincible, invincible, invincible, invincible, invincible, true, invincible);
+            SetEntityCanBeDamaged(vehicle, !invincible);
+            SetVehicleExplodesOnHighExplosionDamage(vehicle, !invincible);
+        }
     }
 }

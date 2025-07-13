4a113bc94f0927e9636819edb037fe1301611dc6
Jon Goodger <jonno.is@gmail.com>
2025-07-13 04:34:58 +0100

    Tidy build scripts

--- build.js
+++ build.js
@@ -1,32 +1,35 @@
 const esbuild = require('esbuild');
 
-const production = 	process.argv.findIndex((argItem) => argItem === '--mode=production') >= 0;
+const production =
+	process.argv.findIndex((argItem) => argItem === '--mode=production') >= 0;
 
-const server = {
-	platform: 'node',
-	target: ['node16'],
-	format: 'cjs',
+const contexts = {
+	server: {
+		platform: 'node',
+		target: ['node16'],
+		format: 'cjs',
+	},
+	client: {
+		platform: 'browser',
+		target: ['chrome93'],
+		format: 'iife',
+	},
 };
 
-const client = {
-	platform: 'browser',
-	target: ['chrome93'],
-	format: 'iife',
-};
-
-for (const context of ['client', 'server']) {
+for (const [context, contextOptions] of Object.entries(contexts)) {
 	esbuild[production ? 'build' : 'context']({
-			bundle: true,
-			entryPoints: [`${context}/${context}.ts`],
-			outfile: `dist/${context}.js`,
-			...(context === 'client' ? client : server),
-		})
+		bundle: true,
+		entryPoints: [`${context}/${context}.ts`],
+		outfile: `dist/${context}.js`,
+		...contextOptions,
+	})
 		.then((r) => {
-			console.log(`[${context}]: Built successfully!`)
+			console.log(`[${context}]: Built successfully!`);
 
 			if (!production) {
-				r.watch()
-					.then(() => console.log(`[${context}]: Watching for changes...`))
+				r.watch().then(() =>
+					console.log(`[${context}]: Watching for changes...`),
+				);
 			}
 		})
 		.catch(() => process.exit(1));
